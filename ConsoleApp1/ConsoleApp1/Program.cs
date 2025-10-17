d44e579919aafae599f93045331243a551c9a60a
hvl9-hash <dossantosdefreitashevellyn9@gmail.com>
2025-10-17 17:08:07 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿//Questão 6
+//using System;
+
+//namespace MediaPonderadaAluno
+//{
+//class Program
+//{
+//static void Main(string[] args)
+//{
+// Solicita as notas do aluno
+//Console.Write("Digite a primeira nota: ");
+//double nota1 = Convert.ToDouble(Console.ReadLine());
+
+//Console.Write("Digite o peso da primeira nota: ");
+//double peso1 = Convert.ToDouble(Console.ReadLine());
+
+//Console.Write("Digite a segunda nota: ");
+//double nota2 = Convert.ToDouble(Console.ReadLine());
+
+//Console.Write("Digite o peso da segunda nota: ");
+//double peso2 = Convert.ToDouble(Console.ReadLine());
+
+//Console.Write("Digite a terceira nota: ");
+//double nota3 = Convert.ToDouble(Console.ReadLine());
+
+//Console.Write("Digite o peso da terceira nota: ");
+//double peso3 = Convert.ToDouble(Console.ReadLine());
+
+// Calcula a média ponderada
+//double mediaPonderada = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3);
+
+//Exibe a média
+//Console.WriteLine($"\nMédia Ponderada: {mediaPonderada:F2}");
+
+// Verifica a situação do aluno
+//if (mediaPonderada >= 7.0)
+//{
+//    Console.WriteLine("Situação: Aprovado");
+// }
+// else if (mediaPonderada >= 5.0)
+//{
+//Console.WriteLine("Situação: Recuperação");
+//}
+//else
+//{
+//Console.WriteLine("Situação: Reprovado");
+//}
+
+// Pausa para visualizar o resultado
+//Console.WriteLine("\nPressione qualquer tecla para sair...");
+//Console.ReadKey();
+//}
+//}
+//}
+
+
